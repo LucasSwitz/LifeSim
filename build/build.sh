e50f7ddd518ec48f35bd07a5429d4763165094c4
pabu <lucswitz@gmail.com>
2017-06-26 12:22:13 -0400

    Moving Functions out of Components and into Scrripts! Moved System's Factory related functions to new class.

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export GTEST_DIR=/home/lucas/Desktop/LifeSim/include/googletest
-export GTEST_LIB_DIR=/home/lucas/Desktop/LifeSim/lib
+export GTEST_DIR=/home/pabu/Desktop/LifeSim/include/googletest
+export GTEST_LIB_DIR=/home/pabu/Desktop/LifeSim/lib
 export LUA_LIB_DIR=/usr/local/lib
 export LUA_INCLUDE_DIR=/usr/local/include
-export LUA_BRIDGE_INCLUDE=/home/lucas/Desktop/LifeSim/include/LuaBridge/Source
+export LUA_BRIDGE_INCLUDE=/home/pabu/Desktop/LifeSim/include/LuaBridge/Source
 export STD_EXPERIMENTAL_LIB=/usr/lib/gcc/x86_64-linux-gnu/5/
-export IMGUI_INCLUDE_DIR=/home/lucas/Desktop/LifeSim/include/imgui
-export IMGUI_SFML_INCLUDE_DIR=/home/lucas/Desktop/LifeSim/include/imgui-sfml
+#export IMGUI_INCLUDE_DIR=/home/pabu/Desktop/LifeSim/include/imgui
+#export IMGUI_SFML_INCLUDE_DIR=/home/pabu/Desktop/LifeSim/include/imgui-sfml
 
 for arg in "$@"
 do
@@ -21,7 +21,7 @@ do
     esac
 done
 
-export GTEST_OUTPUT="xml:/home/lucas/Desktop/LifeSim/test/output/test_output"
+export GTEST_OUTPUT="xml:/home/pabu/Desktop/LifeSim/test/output/test_output"
 make
 ./LifeSim-Test
 
